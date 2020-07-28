6ba0f7b40466633754116d6e12f11d4a0baca70f
Philippe Rougerie <philipperougerie14@gmail.com>
2020-07-28 22:12:34 +0200

    Inclusion d'une vm pour gerer la gamePage.

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:super_game_app/viewmodels/connexion_view_model.dart';
+import 'package:super_game_app/viewmodels/display_view_model.dart';
 import 'package:super_game_app/viewmodels/gameListViewmodel.dart';
 import 'package:super_game_app/viewmodels/gameViewModel.dart';
 
@@ -13,24 +13,33 @@ class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(game.name),
-      ),
-      body: Column(
-        children: [
-           Text(
-             game.name,
-             style: TextStyle(fontSize: 23,color: Color.fromRGBO(125, 166, 125, 1)),
-              ),
-          Text(
-            game.deck
+    return  ChangeNotifierProvider<DisplayGameViewModel>(
+        create: (context) => DisplayGameViewModel(game),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(game.name),
           ),
-          Image(
-            image: NetworkImage(game.image["medium_url"].toString())
-          )
-        ],
-      ),
+          body:
+          Consumer<DisplayGameViewModel>(
+          builder: (context,vm, child){
+            return Column(
+              children: [
+                Text(
+                  vm.game.name,
+                  style: TextStyle(fontSize: 23,color: Color.fromRGBO(125, 166, 125, 1)),
+                ),
+                Text(
+                    vm.game.deck
+                ),
+                Image(
+                    image: NetworkImage(vm.game.image["medium_url"].toString())
+                )
+              ],
+            );
+          }
+
+        )
+    )
     );
   }
 }
